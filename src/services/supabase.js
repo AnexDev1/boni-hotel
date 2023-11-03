031e79953f87a6fed329d459c09200f074b21886
Anexdev1 <Anwarnasir0970@gmail.com>
2023-11-03 01:37:14 -0900

    built backend with supabase and connected to my app

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://vmgaepjtmrddqmyowphl.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZtZ2FlcGp0bXJkZHFteW93cGhsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTg5OTg0NzAsImV4cCI6MjAxNDU3NDQ3MH0.rS53KMx3Izw6s9FQ5zmdoDoxsyupVZMgjjuBWO0FH60";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
