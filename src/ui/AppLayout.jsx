61a90368796276a604ba7153a807ad55c6e14c58
Anexdev1 <Anwarnasir0970@gmail.com>
2023-11-02 17:30:44 -0900

    started working with applayout

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,15 +1,27 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import styled from "styled-components";
 
+const Main = styled.main`
+  background-color: var(--color-grey-50);
+  padding: 4rem 4.8rem;
+`;
+const StyledAppLayout = styled.div`
+  display: grid;
+  grid-template-columns: 26rem 1fr;
+  grid-template-rows: auto 1fr;
+  height: 100vh;
+`;
 function AppLayout() {
   return (
-    <div>
-      <p>AppLayout</p>
+    <StyledAppLayout>
       <Header />
       <Sidebar />
-      <Outlet />
-    </div>
+      <Main>
+        <Outlet />
+      </Main>
+    </StyledAppLayout>
   );
 }
 
