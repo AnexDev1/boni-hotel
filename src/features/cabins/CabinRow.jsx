619b794f9f27d036db56e5fd5a1fd6b84cc69554
anexdev1 <anwarnasir0970@gmail.com>
2023-12-16 04:52:42 -0900

    created a re-usable table body

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,15 +1,15 @@
 import styled from "styled-components";
-import { formatCurrency } from "../../utils/helpers";
+import { HiMiniPencilSquare } from "react-icons/hi2";
+import { HiClipboardDocumentCheck } from "react-icons/hi2";
+import { HiOutlineTrash } from "react-icons/hi2";
 
+import { formatCurrency } from "../../utils/helpers";
 import CreateCabinForm from "./CreateCabinForm";
 import { useDeleteCabin } from "./useDeleteCabin";
 import { useCreateCabin } from "./useCreateCabin";
 import Modal from "../../ui/Modal";
 import ConfirmDelete from "../../ui/ConfirmDelete";
 import Table from "../../ui/Table";
-import { HiClipboardDocumentCheck } from "react-icons/hi2";
-import { HiMiniPencilSquare } from "react-icons/hi2";
-import { HiOutlineTrash } from "react-icons/hi2";
 
 const Img = styled.img`
   display: block;
