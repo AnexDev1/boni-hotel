e1b539ef0f2071298f7c54d487a7405e7023cbbe
anexdev1 <anwarnasir0970@gmail.com>
2023-12-19 11:26:39 +0300

    implemented filtering on the database

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,10 +1,22 @@
+import { useSearchParams } from "react-router-dom";
 import { getBookings } from "../../services/apiBookings";
 import { useQuery } from "@tanstack/react-query";
 
 export function useBookings() {
-  const { data: bookings, isLoading, error } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getBookings,
+  const [searchParams] = useSearchParams();
+
+  const filterValue = searchParams.get("status");
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : { field: "status", value: filterValue, method: "eq" };
+  const {
+    data: bookings,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["bookings", filter],
+    queryFn: () => getBookings({ filter }),
   });
   return { isLoading, bookings, error };
 }
