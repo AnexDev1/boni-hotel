7e428734c47da9b953e02303f24e10cc4844d376
anexdev1 <anwarnasir0970@gmail.com>
2023-12-20 10:37:15 +0300

    implemented sorting from the api-side on supabase

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -5,18 +5,26 @@ import { useQuery } from "@tanstack/react-query";
 export function useBookings() {
   const [searchParams] = useSearchParams();
 
+  //FILTER
   const filterValue = searchParams.get("status");
   const filter =
     !filterValue || filterValue === "all"
       ? null
       : { field: "status", value: filterValue, method: "eq" };
+
+  //SORTING
+  const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
+  const [field, direction] = sortByRaw.split("-");
+
+  const sortBy = { field, direction };
+
   const {
     data: bookings,
     isLoading,
     error,
   } = useQuery({
-    queryKey: ["bookings", filter],
-    queryFn: () => getBookings({ filter }),
+    queryKey: ["bookings", filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
   return { isLoading, bookings, error };
 }
--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -5,8 +5,13 @@ export async function getBookings({ filter, sortBy }) {
   let query = supabase
     .from("bookings")
     .select("*, cabins(name), guests(fullName, email)");
-  if (filter !== null)
-    query = query[filter.method || "eq"](filter.field, filter.value);
+  if (filter) query = query[filter.method || "eq"](filter.field, filter.value);
+
+  if (sortBy)
+    query = query.order(sortBy.field, {
+      ascending: sortBy.direction === "asc",
+    });
+
   const { data, error } = await query;
   if (error) {
     console.error("bookings could not be loaded");
