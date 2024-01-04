a0f32259a02e1ea42d7133b0d6a7b7a580859b8b
anexdev1 <anwarnasir0970@gmail.com>
2024-01-04 10:54:49 +0300

    implemented changing user name and password

---
+++ src/features/authentication/useUpdateUser.js
@@ -0,0 +1,23 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { updateCurrentUser } from "../../services/apiAuth";
+import { toast } from "react-hot-toast";
+
+export function useUpdateUser() {
+  const queryClient = useQueryClient();
+
+  const { isLoading: isUpdating, mutate: updateUser } = useMutation({
+    mutationFn: updateCurrentUser,
+    onSuccess: () => {
+      toast.success("User account successfully Updated");
+      // queryClient.setQueryData("user", user);
+      queryClient.invalidateQueries({
+        queryKey: ["user"],
+      });
+      // reset();
+    },
+    onError: (err) => {
+      toast.error(err.message);
+    },
+  });
+  return { isUpdating, updateUser };
+}
